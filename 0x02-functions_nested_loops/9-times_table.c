6c5d5db264e32d597c44cd10bec77f4b8a9ccf87
ZohraAS <zohraaitsaid06@gmail.com>
2023-04-26 16:20:23 -0700

    hakra yadn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
-	}
+	{
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
@@ -29,7 +29,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-			}
+				}
 		}
 		_putchar('\n');
 	}
